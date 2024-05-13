d8c35477eb78e1c79f17d2f0b7696fdae0a2ef6e
markbus-ai <58405544+markbus-ai@users.noreply.github.com>
2024-05-13 11:18:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-import uvicorn
-from os import getenv
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-if __name__ == "__main__":
-    port =int(getenv("PORT", 8000))
-    uvicorn.run("app.api:app",host="0.0.0.0", port=port,reload=True)
+app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins origins,
+    allow credentials=True,
+    allow_methods=["+"],
+    allow_headers=["*"],
+)
+app.get("/")
+def read root():
+    return "Welcome to shorten url api"
